b470574e2da7a573006de2ae5aa9308570350a6a
yumigm <110480295+yumigm@users.noreply.github.com>
2022-08-04 15:10:25 -0400

    Update PersistenceConfiguration.java

--- src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
+++ src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
@@ -12,13 +12,13 @@ import javax.sql.DataSource;
 
 @Configuration
 public class PersistenceConfiguration {
-    @Bean
-    public DataSource dataSource(){
-        DataSourceBuilder builder = DataSourceBuilder.create();
-        builder.url("jdbc:postgresql://localhost:5432/conference_app");
-        System.out.println("Custom DataSource Set");
-        return builder.build();
-    }
+//     @Bean
+//     public DataSource dataSource(){
+//         DataSourceBuilder builder = DataSourceBuilder.create();
+//         builder.url("jdbc:postgresql://localhost:5432/conference_app");
+//         System.out.println("Custom DataSource Set");
+//         return builder.build();
+//     }
 
 
 }
